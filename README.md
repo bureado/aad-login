77f3538c91eeda56c3879e54cc4811e92db2318c
Jose Miguel Parrella <j@bureado.com>
2015-04-15 02:50:47 +0000

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # aad-login
-allows Linux user authentication to Azure AD via pam_exec
+
+Allows Linux user authentication to Azure AD via pam_exec
+
+## Prerequisites
+
+* An Azure AD directory has been created, and some users exist
+* Node.js and npm are installed in the Linux VM
+* A directory application has been created (native client type) and you have the Client ID
+
+## User provisioning
+
+This utility doesn't provision the user. In other words, you need to ensure the user
+you'll be logging in with is visible by NSS. A simple `sudo useradd -m <user>` might
+be enough for a handful of users.
+
+## Installing
+
+You can download the tarfile and:
+
+    sudo tar xzf aad-login_0.1.tar.gz
+    cd /opt/aad-login
+    sudo npm install
+
+## Configuring
+
+First, open `/opt/aad-login/aad-login.js` with your favorite editor and put your directory
+and client ID in.
+
+Then, open `/etc/pam.d/common-auth` and add:
+
+    auth sufficient pam_exec.so expose_authtok /usr/local/bin/aad-login
+
+
+
+
+
---
+++ aad-login.js
@@ -0,0 +1,41 @@
+// Configuration parameters
+var directory = '';
+var clientid  = '';
+
+if (!directory || !clientid) {
+  console.log('You need to provide your directory and client ID');
+  process.exit(1);
+}
+
+var username = process.argv[2];
+var password = process.argv[3];
+
+if (username && password) {
+  request = {
+    tenant : directory,
+    authorityHostUrl : 'https://login.windows.net',
+    clientId : clientid,
+    username : username + '@' + directory,
+    password : password
+  };
+
+  var adal = require('adal-node');
+  var AuthenticationContext = adal.AuthenticationContext;
+  var authorityUrl = request.authorityHostUrl + '/' + request.tenant;
+  var resource = '00000002-0000-0000-c000-000000000000';
+  var context = new AuthenticationContext(authorityUrl);
+
+  context.acquireTokenWithUsernamePassword(resource, request.username, request.password, request.clientId, function(err, tokenResponse) {
+    if (err) { // fail
+        console.log(tokenResponse);
+        process.exit(1);
+    } else {
+        console.log(tokenResponse);
+        process.exit(0);
+    }
+  });
+} else {
+  console.log('No username/password provided');
+  process.exit(1);
+}
+
